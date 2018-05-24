4199cbe042ea379b138267b0763fa157bce00dc1
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-24 10:50:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,11 @@ class Application
     current_hour = current_time.strftime("%H").to_i
     current_min = current_time.strftime("%M").to_i
 
-    if time_now > 12:00
-      resp
+    if current_hour >= 12 && current_min >= 0
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end #if current_hour/min
 
   end #call
 
